3a8a2d943f878b9bc87d3057f2f9d99c219a4ef2
Mohit Singhal <singhalmohit011@gmail.com>
2025-09-13 09:37:17 +0530

    Enabled headless mode

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def main():
 
     # Login to Naukri
     chrome_options = webdriver.ChromeOptions()
-    # chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
